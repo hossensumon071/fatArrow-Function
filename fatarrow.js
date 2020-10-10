6ebda955dfe1aa4bc07e3dfdcfb5669c79dba49f
hossensumon071 <hossensumon071@gmail.com>
2020-10-10 01:49:21 -0700

    practice fat arrw fucnton

---
+++ fatarrow.js
@@ -0,0 +1,26 @@
+// Es6 fat arrow function
+
+// function number() {
+//     return 10;
+// }
+
+// let number = () => 10;
+
+// console.log(number());
+
+// var javascript = {
+//     name: 'javascript',
+//     libraries: ['React', 'Angular', 'Vue'],
+//     priintLibraries: function() {
+//         this.libraries.forEach((a) => console.log(`${this.name} loves ${a}`),
+//         ) 
+//     },
+// };
+
+// javascript.priintLibraries();
+
+function Person(name) {
+    this.name = name;
+}
+
+var sakib = new Person('sakib');
